f8b73f4bc36d982836009b49ca3769f35335753f
Katrin <katrinivanovaa@yahoo.com>
2025-03-08 15:28:14 +0000

    added lottie through js.
    temperature change works

--- src.js/index.js
+++ src.js/index.js
@@ -19,7 +19,45 @@ function refreshWeather(response) {
   windDisplay.innerHTML = `${windSpeedMph} m/h`;
   farenheitElement.innerHTML = `${temperatureFarenheit}°F`;
 
+  updateLottieAnimation(temperatureCelsius);
+
   console.log(response.data);
+}
+function updateLottieAnimation(temp) {
+  let lottieContainer = document.querySelector("#lottie-container");
+
+  // If the container doesn't exist, create it inside the header
+  if (!lottieContainer) {
+    lottieContainer = document.createElement("div");
+    lottieContainer.id = "lottie-container";
+    document.querySelector("header").prepend(lottieContainer);
+  }
+
+  // Clear any previous Lottie animation
+  lottieContainer.innerHTML = "";
+
+  // Choose the correct Lottie animation based on temperature
+  let lottieSrc;
+  if (temp > 17) {
+    lottieSrc =
+      "https://lottie.host/de906e4c-5d3b-4185-a33a-3cf3a3af1c72/kopk9gEQQt.lottie"; // Warm icon
+  } else {
+    lottieSrc =
+      "https://lottie.host/c2038e32-5c7e-4746-b389-f93f60261f1d/b2GsH7cm7d.lottie"; // Cold icon
+  }
+
+  // Create the Lottie player element
+  let lottiePlayer = document.createElement("dotlottie-player");
+  lottiePlayer.src = lottieSrc;
+  lottiePlayer.background = "transparent";
+  lottiePlayer.speed = "1";
+  lottiePlayer.style.width = "300px";
+  lottiePlayer.style.height = "300px";
+  lottiePlayer.loop = true;
+  lottiePlayer.autoplay = true;
+
+  // Add the Lottie animation to the container
+  lottieContainer.appendChild(lottiePlayer);
 }
 
 function searchCity(city) {
